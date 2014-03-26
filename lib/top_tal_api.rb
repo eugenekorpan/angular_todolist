93b5e94076b6da2fa77333c98642d2c36b980f80
Eugene Korpan <eugene@active-bridge.com>
2014-03-26 09:31:57 +0200

    Added API Access Token expiration time

--- lib/top_tal_api.rb
+++ lib/top_tal_api.rb
@@ -1,7 +1,14 @@
 module TopTalApi
 
+  TOKEN_EXPIRATION_TIME = 2.hours
+
   def self.find_user_by token
-    User.joins(:access_token).where('access_tokens.token = ?', token).first
+    user = User.joins(:access_token).where('access_tokens.token = ?', token).first
+    if user && user.access_token.created_at < Time.now - TOKEN_EXPIRATION_TIME
+      user.access_token.destroy
+      return nil
+    end
+    user
   end
 
   class Client
