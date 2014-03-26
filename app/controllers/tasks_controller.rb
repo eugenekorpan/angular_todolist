ca7ca4e103a31644b06a09fbf68434feb1668016
Eugene Korpan <eugene@active-bridge.com>
2014-03-26 07:54:02 +0200

    Add ability to sort list by priority or due date

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,10 @@ class TasksController < ApplicationController
   respond_to :json
 
   before_filter :find_task, only: [:update, :destroy]
+  before_filter :setup_order, only: :index
 
   def index
-    tasks = current_user.tasks.order('id asc')
+    tasks = current_user.tasks.order(@order)
     tasks = tasks.send(params[:status]) if params[:status]
     respond_with tasks
   end
@@ -30,5 +31,11 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :completed, :priority, :due_date)
+  end
+
+  def setup_order
+    order_by  = params[:order_by] || 'priority'
+    direction = params[:direction] || 'asc'
+    @order = "#{order_by} #{direction}"
   end
 end
